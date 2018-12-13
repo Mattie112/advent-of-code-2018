1e0340626cf1d04a879e7a61195a6efcef1bca73
Matthijs <Mattie112@users.noreply.github.com>
2018-12-13 20:01:20 +0100

    Day 12 finished

--- day12/day12.php
+++ day12/day12.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * https://adventofcode.com/2018/day/11
+ * https://adventofcode.com/2018/day/12
  */
 
-// todo add isset's to prevent notices
-error_reporting(0);
 $initial = "";
 $notes = [];
 
@@ -26,7 +24,7 @@ if ($file = fopen(__DIR__ . "/day12-input.txt", "rb")) {
     }
 }
 
-// Start by adding the 'left' lots
+// Start by adding the 'left' pots
 $pots = [];
 $pots[-3] = ".";
 $pots[-2] = ".";
@@ -47,54 +45,79 @@ $pots[] = ".";
 $pots[] = ".";
 
 $next_iter_pots = $pots;
-$generations = 20;
+$generations = 50000000000 - 1;
 $count = 0;
 $number_count = 0;
+$part1 = 0;
+$part2 = 0;
+$prev_answer = 0;
+$prev_plants = "";
 
-for ($i = 0; $i < $generations; $i++) {
+for ($i = 0; $i <= $generations; $i++) {
     foreach ($pots as $index => $pot) {
         // We simply use the string to check the nodes
         $str_to_check = "";
-        $str_to_check .= $pots[$index - 2];
-        $str_to_check .= $pots[$index - 1];
+        $str_to_check .= $pots[$index - 2] ?? ".";
+        $str_to_check .= $pots[$index - 1] ?? ".";
         $str_to_check .= $pots[$index];
-        $str_to_check .= $pots[$index + 1];
-        $str_to_check .= $pots[$index + 2];
+        $str_to_check .= $pots[$index + 1] ?? ".";
+        $str_to_check .= $pots[$index + 2] ?? ".";
 
-        if (isset($notes[$str_to_check])) {
-            $next_iter_pots[$index] = $notes[$str_to_check];
-        } else {
-            $next_iter_pots[$index] = ".";
-        }
+        $next_iter_pots[$index] = $notes[$str_to_check] ?? ".";
     }
 
     // Add more pots to the left when needed
-    reset($pots);
-    $first_key = key($pots);
-    if ($pots[$first_key + 2] === "#") {
+    reset($next_iter_pots);
+    $first_key = key($next_iter_pots);
+    if ($next_iter_pots[$first_key + 2] === "#") {
         $next_iter_pots[$first_key - 1] = ".";
         ksort($next_iter_pots);
     }
 
     // As we have negative indexes we'll need to know the last element
-    end($pots);
-    $last_key = key($pots);
+    end($next_iter_pots);
+    $last_key = key($next_iter_pots);
     // If we end with a plant add more pots (that is how I read the example)
-    if ($pots[$last_key - 2] === "#") {
+    if ($next_iter_pots[$last_key - 2] === "#") {
         $next_iter_pots[] = ".";
     }
+
     // Store the next iteration into the current iteration
     $pots = $next_iter_pots;
-    $count += array_count_values($pots)["#"];
-    echo $count . PHP_EOL;
-}
+    $answer = calcAnswer($pots);
+    echo implode("", $pots) . PHP_EOL;
 
-// Count the numbers, not the plants for part 1
-$number_count = 0;
-foreach ($pots as $index => $pot) {
-    if ($pot === "#") {
-        $number_count += $index;
+    if ($i === 20) {
+        $part1 = $answer;
     }
+
+    // In the end it seems that we are only "moving" our plants and not adding new
+    // so if we take only the pots that contain a plant and check that we know if thare are any differences
+    $first_plant = array_search("#", $pots, true);
+    $plants = substr(implode("", $pots), $first_plant);
+    if ($prev_plants === $plants && $i > 20) {
+        // If not then we are not doing anything usefull and we can calculate the answer without looping YEAH!
+        // So: our current answer + the diff between the last to (that is added each generation) but substract the current generation as that is already in our $answer
+        $part2 = $answer + (($answer - $prev_answer) * ($generations - $i));
+        break;
+    }
+
+    $prev_plants = $plants;
+    $prev_answer = $answer;
 }
 
-echo PHP_EOL . "Part #1: " . $number_count . PHP_EOL;
+function calcAnswer($pots)
+{
+    $number_count = 0;
+    foreach ($pots as $index => $pot) {
+        if ($pot === "#") {
+            $number_count += $index;
+        }
+    }
+
+    return $number_count;
+}
+
+
+echo PHP_EOL . "Part #1: " . $part1 . PHP_EOL;
+echo PHP_EOL . "Part #2: " . $part2 . PHP_EOL;
